4cdff3a787913bdb7d0a734ea5be5b9fb877d04d
Carlos Guillermo Jiménez Salcedo <guillermo.jimenez@yuju.io>
2021-12-19 21:26:08 -0700

    API endpoints enhanced

--- back/src/resources/branches.py
+++ back/src/resources/branches.py
@@ -1,18 +1,28 @@
 from datetime import datetime
 from typing import Dict, Iterable, Union
 
-from flask_restful import Resource
+from flask_restful import request, Resource
 from git_manager.repo import Repository
+from git_manager.exceptions import NoBranch
+
+BranchType = Dict[str, Union[str, datetime]]
 
 
 class Branches(Resource):
     def __init__(self) -> None:
         self._repo_manager = Repository()
 
-    def get(self) -> Iterable[Dict[str, Union[str, datetime]]]:
-        """Return all branches in the repository
+    def get(self) -> Union[BranchType, Iterable[BranchType]]:
+        """Return branches information
 
         Returns:
             List of branches
         """
-        return self._repo_manager.get_branches()
+        branch_name = request.args.get('name')
+        if branch_name:
+            try:
+                return self._repo_manager.get_branch(branch_name)
+            except NoBranch as ex:
+                return {'message': str(ex)}, 404
+        else:
+            return self._repo_manager.get_branches()
